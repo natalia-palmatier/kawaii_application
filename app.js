d4495191880f500d2506582f8d5b97552ca47015
Natalia Palmatier <87651358+natalia-codes@users.noreply.github.com>
2022-10-25 16:27:18 -0700

    install pug style

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const colors = require('colors')
 
 const server = app.listen(port, () => console.log('server listening on port '.rainbow + port));
 
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
 app.get('/', (req, res, next) => {
-    res.status(200).send('test');
+    res.status(200).render('home');
 })
