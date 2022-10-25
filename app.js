2f38be18f715ab115d30889cacd47d3bc0847441
Natalia Palmatier <87651358+natalia-codes@users.noreply.github.com>
2022-10-25 16:59:15 -0700

    added main-layout page

--- app.js
+++ app.js
@@ -10,5 +10,11 @@ app.set('view engine', 'pug');
 app.set('views', 'views');
 
 app.get('/', (req, res, next) => {
-    res.status(200).render('home');
+
+    var payload = {
+        pageTitle: 'Home'
+    }
+
+
+    res.status(200).render('home', payload);
 })
