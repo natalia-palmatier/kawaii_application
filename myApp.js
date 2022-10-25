8fd3a709bc6ee71e2d7357452f689db6f86a9807
Natalia Palmatier <87651358+natalia-codes@users.noreply.github.com>
2022-10-25 15:07:43 -0700

    testing content

--- myApp.js
+++ myApp.js
@@ -1,7 +1,10 @@
 var http = require('http');
 
 var server = http.createServer((req, res) => {
-    console.log("Joined!");
+    // console.log("Joined!");
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<p>test content</p>');
+    res.end();
 })
 
 server.listen(3001);
