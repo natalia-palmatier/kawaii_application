f8f189df89c2832c98926114258d174fa3037f49
Natalia Palmatier <87651358+natalia-codes@users.noreply.github.com>
2022-10-25 15:39:12 -0700

    test post/get requests

--- routes.js
+++ routes.js
@@ -1,18 +1,31 @@
 const displayContent = (req, res) => {
     const url = req.url;
+    const method = req.method;
 
-    if (url == '/profile') {
+    if(url == "/profile") {
         res.setHeader('Content-Type', 'text/html');
-        res.write('<p> progile page test </p>');
+        res.write('<p>profile</p>');
         return res.end();
     }
-    else if (url == '/') {
+    else if (url == "/") {
         res.setHeader('Content-Type', 'text/html');
-        res.write('<p> homepage test </p>');
+        res.write('<p>home</p>');
         return res.end();
     }
+    else if (url == "/settings" && method == "POST") {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<h1>submitted</h1>');
+        return res.end();
+    }
+    else if (url == "/settings") {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<h1>Settings</h1><form action="/settings" method="POST"><input type="text"><button type="submit">submit</button></form>');
+        return res.end();
+    }
+
+
     res.setHeader('Content-Type', 'text/html');
-    res.write('<p> no page </p>');
+    res.write('<p>no page</p>');
     res.end();
 }
 
