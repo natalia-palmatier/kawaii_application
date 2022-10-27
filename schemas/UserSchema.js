3e62d33d51219013da6e7812ee753781d3ee46d8
Natalia Palmatier <87651358+natalia-codes@users.noreply.github.com>
2022-10-27 14:47:59 -0700

    adding retweet field to schema

--- schemas/UserSchema.js
+++ schemas/UserSchema.js
@@ -33,7 +33,8 @@ const UserSchema = new Schema({
         type: String,
         default: "/images/default.png"
     }, 
-    likes: [{ type: Schema.Types.ObjectId, ref: 'Post' }]
+    likes: [{ type: Schema.Types.ObjectId, ref: 'Post' }],
+    retweets: [{ type: Schema.Types.ObjectId, ref: 'Post' }]
 }, { timestamps: true });
 
 var User = mongoose.model('User', UserSchema);
