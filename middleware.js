e3461e07910e1cdb9261a0adff1521f24cfddf81
Natalia Palmatier <87651358+natalia-codes@users.noreply.github.com>
2022-11-15 20:42:05 -0800

    code refactor

--- middleware.js
+++ middleware.js
@@ -1,10 +1,8 @@
 exports.requireLogin = (req, res, next) => {
-    // iff session = OK & user = OK, login, if not: redirect to login page
     if (req.session && req.session.user) {
         return next();
-
     }
     else {
-        return res.redirect('/login')
+        return res.redirect('/login');
     }
 }
